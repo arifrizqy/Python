53838123ef4b5d91029506cc882ce9e9bd7652a6
Arif Rizqy <107555247+arifrizqy@users.noreply.github.com>
2022-09-14 17:07:55 +0700

    Add file volumeCalculator-BlockandCube.py
    Adding file volumeCalculator-BlockandCube.py

---
+++ volumeCalculator-BlockandCube.py
@@ -0,0 +1,7 @@
+name = "Muhammad Arif Rizqy Fachrudin"
+nim = 220441100015
+height = 1.7
+
+print("Variable name berisi \t\t : {}".format(name))
+print("Variable nim berisi \t\t : {}".format(nim))
+print("Variable tinggi_badan berisi \t : {}".format(height))
